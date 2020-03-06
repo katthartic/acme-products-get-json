a943d03bb4821d8573d07004c2138c15ac9973f8
katthartic <katt.baum@gmail.com>
2020-03-05 21:50:10 -0500

    Add functionality

--- server.js
+++ server.js
@@ -3,3 +3,16 @@ const path = require('path')
 const db = require('./db')
 const app = express()
 const port = process.env.PORT || 3000
+
+app.get('/', (req, res, next) => {
+  console.log(`HITTING: ${req.url} - ${req.method}`)
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/api/products', (req, res, next) => {
+  db.readJSON('products.json')
+    .then(products => res.send(products))
+    .catch(next)
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
