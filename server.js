3260964f35a35abcff724ed60400f34b219ea8c7
katthartic <katt.baum@gmail.com>
2020-03-06 10:40:52 -0500

    Add companines

--- server.js
+++ server.js
@@ -5,13 +5,21 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res, next) => {
-  console.log(`HITTING: ${req.url} - ${req.method}`)
+  console.log(`Hitting: ${req.url} - ${req.method}`)
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.get('/api/products', (req, res, next) => {
+  console.log(`Hitting: ${req.url} - ${req.method}`)
   db.readJSON('products.json')
     .then(products => res.send(products))
+    .catch(next)
+})
+
+app.get('/api/companies', (req, res, next) => {
+  console.log(`Hitting: ${req.url} - ${req.method}`)
+  db.readJSON('companies.json')
+    .then(companies => res.send(companies))
     .catch(next)
 })
 
